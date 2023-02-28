2e3587a0f22c613ad8c2496f40f368edbb392dc8
LelouchHe <lelouchhe@gmail.com>
2023-02-28 15:45:50 -0800

    update format

--- vite.config.ts
+++ vite.config.ts
@@ -28,23 +28,17 @@ export default defineConfig({
       querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
       punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
       url: 'rollup-plugin-node-polyfills/polyfills/url',
-      string_decoder:
-        'rollup-plugin-node-polyfills/polyfills/string-decoder',
+      string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
       http: 'rollup-plugin-node-polyfills/polyfills/http',
       https: 'rollup-plugin-node-polyfills/polyfills/http',
       os: 'rollup-plugin-node-polyfills/polyfills/os',
       assert: 'rollup-plugin-node-polyfills/polyfills/assert',
       constants: 'rollup-plugin-node-polyfills/polyfills/constants',
-      _stream_duplex:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
-      _stream_passthrough:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
-      _stream_readable:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
-      _stream_writable:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
-      _stream_transform:
-        'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
+      _stream_duplex: 'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
+      _stream_passthrough: 'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
+      _stream_readable: 'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
+      _stream_writable: 'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
+      _stream_transform: 'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
       timers: 'rollup-plugin-node-polyfills/polyfills/timers',
       console: 'rollup-plugin-node-polyfills/polyfills/console',
       vm: 'rollup-plugin-node-polyfills/polyfills/vm',
